5b06f8e3d4251e0d232ba17e4771ac731531b94b
Gabriel Koehler da Silva <gabrielks@weg.net>
2023-09-27 21:54:54 -0300

    fix resolve

--- components/BuscarMaisComponent.jsx
+++ components/BuscarMaisComponent.jsx
@@ -1,8 +1,3 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
-
 export default function BuscarMaisComponent(){
     return(
         <>
